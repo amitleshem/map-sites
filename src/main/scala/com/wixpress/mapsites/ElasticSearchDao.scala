0b7c42a32c70042e719ccc0c94daf3ee9317e2fc
Amit <amitle@wix.com>
2017-08-13 12:57:41 +0300

    added mapping to geo_point

--- src/main/scala/com/wixpress/mapsites/ElasticSearchDao.scala
+++ src/main/scala/com/wixpress/mapsites/ElasticSearchDao.scala
@@ -3,7 +3,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.wixpress.authorization.MetaSiteId
 import com.wixpress.hoopoe.ids.Guid
 import com.wixpress.hoopoe.json.JsonMapper
-import com.workday.esclient.EsClient
+import com.wixpress.siteproperties.api.v2.GeoCoordinates
+import com.workday.esclient.{EsClient, IndexFieldProperties, IndexMappings, IndexTypeMappings}
 import net.liftweb.json.DefaultFormats
 import net.liftweb.json.Serialization.write
 
@@ -17,23 +18,40 @@ class ElasticSearchDao(esUrl: String, indexName: String) extends Dao{
   val mapper = JsonMapper.objectMapperFromTemplate
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
-  Try{client.catIndex(indexName)}.recover{ case _ => client.createIndex(indexName) }.get
+  val settings = Map("mappings" -> Map("site" -> Map("properties" -> Map("coordinates" -> Map("type" -> "geo_point")))))
 
-  private def deserialize(payload: Array[Byte]): Address =
-    mapper.readValue(payload, classOf[Address])
+  Try{client.catIndex(indexName)}.recover{ case _ => client.createIndex(indexName, Some(settings)) }.get
+
+  private def deserialize(payload: Array[Byte]): ESAddress =
+    mapper.readValue(payload, classOf[ESAddress])
+
+  def esGeoPointToGeoCoordinates(eSGeoPoint: ESGeoPoint) = GeoCoordinates(eSGeoPoint.lat, eSGeoPoint.lon)
+
+  private def esAddressToAddress(eSAddress: ESAddress): Address = Address(eSAddress.street, eSAddress.city, eSAddress.country, eSAddress.coordinates.map(esGeoPointToGeoCoordinates))
 
   override def delete(metasiteId: Guid[_]): Unit = ??? //implement if needed
 
   override def get(metaSiteId: Guid[_]): Option[Address] = {
     val getDoc = client.get(indexName, metaSiteId.getId)
-    getDoc.get.sourceJson.map(_.toString.getBytes).map(deserialize)
+    getDoc.get.sourceJson
+      .map(_.toString.getBytes)
+      .map(deserialize)
+      .map(esAddressToAddress)
   }
 
   override def put(metaSiteId: MetaSiteId, address: Address): Unit = {
     implicit val formats = DefaultFormats
-    val jsonString = write(address)
+    val jsonString = write(convertToES(address))
     val id = metaSiteId.getId
     client.index(indexName, typeName, id, jsonString)
   }
 
+  private def convertToES(address: Address): ESAddress =
+    ESAddress(address.street, address.city, address.country, address.coordinates.map(geoCoordinatesToESGeoPoint))
+
+  private def geoCoordinatesToESGeoPoint(c: GeoCoordinates) = ESGeoPoint(c.latitude, c.longitude)
+
 }
+
+case class ESGeoPoint(lat: Double, lon: Double)
+case class ESAddress(street: String, city: String, country: Int, val coordinates: Option[ESGeoPoint])
